15a304ecf215385332881b58c964d29e932d8835
Tommy <tommy.nguyen0721@gmail.com>
2024-09-16 21:22:01 -0400

    Flappy Bird Tutorial

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidBody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true && birdIsAlive)
+        {
+
+            myRigidBody.velocity = Vector2.up * flapStrength;
+        }
+        if (myRigidBody.position.y < -12 || myRigidBody.position.y > 12)
+        {
+            birdIsAlive = false;
+            logic.gameOver();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        birdIsAlive = false;
+        logic.gameOver();
+    }
+
+}
